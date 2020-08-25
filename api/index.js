6a31d65cffc1fbd795bf65cea745a348751d5408
Oleg Mihailik <mihailik@gmail.com>
2020-08-25 14:47:52 +0100

    See if it works with functions and netlify.toml.

---
+++ api/index.js
@@ -0,0 +1,3 @@
+module.exports = async function (event, context) {
+    return 'OK done.';
+};
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 (async () => {
-    const start = new Date();
-    document.body.style.cssText =
-        `
-            position: fixed;
-            left: 0; top: 0; width: 100%; height: 100%;
-            overflow: hidden;
-            display: grid;
-            grid-template-rows: 1fr;
-            grid-template-columns: 1fr;
-            align-items: center;
-            justify-items: center;
-        `;
-    const el = document.createElement('pre');
-    el.style.cssText =
-        `
-            font-size: 500%;
-        `;
-    document.body.appendChild(el);
-    for (let i = 0; i < 5; i++) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        el.textContent = document.title = `Loading ${5 - i}...`;
-    }
+  const start = new Date();
+  document.body.style.cssText =
+    `
+      position: fixed;
+      left: 0; top: 0; width: 100%; height: 100%;
+      overflow: hidden;
+      display: grid;
+      grid-template-rows: 1fr;
+      grid-template-columns: 1fr;
+      align-items: center;
+      justify-items: center;
+    `;
+  const el = document.createElement('pre');
+  el.style.cssText =
+    `
+      font-size: 500%;
+    `;
+  document.body.appendChild(el);
+  for (let i = 0; i < 5; i++) {
     await new Promise(resolve => setTimeout(resolve, 1000));
-    const finish = new Date();
-    el.textContent = document.title = 'DONE!';
+    el.textContent = document.title = `Loading ${5 - i}...`;
+  }
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  const finish = new Date();
+  el.textContent = document.title = 'DONE!';
 
-    // extra timings output...
-    const details = document.createElement('div');
-    details.style.cssText = 'font-size: 25%';
-    details.textContent = finish + ' (' + ((finish.getTime() - start.getTime()) / 1000) + 's)';
-    el.appendChild(details);
+  // extra timings output...
+  const details = document.createElement('div');
+  details.style.cssText = 'font-size: 25%';
+  details.textContent = finish + ' (' + ((finish.getTime() - start.getTime()) / 1000) + 's)';
+  el.appendChild(details);
 })();
