44f4d92cf4ae112338a33bd73a16e5ff0de00c31
Oleg Mihailik <mihailik@gmail.com>
2020-08-25 14:27:42 +0100

    Bare page with minimal script.

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+(async () => {
+    document.body.style.cssText =
+        `
+            position: fixed;
+            left: 0; top: 0; width: 100%; height: 100%;
+            overflow: hidden;
+            display: grid;
+            grid-template-rows: 1fr;
+            grid-template-columns: 1fr;
+            align-items: center;
+            justify-items: center;
+        `;
+    const el = document.createElement('pre');
+    el.style.cssText =
+        `
+            font-size: 500%;
+        `;
+    document.body.appendChild(el);
+    for (let i = 0; i < 5; i++) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        el.textContent = `Loading ${5 - i}...`;
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    el.textContent = 'DONE!';
+})();
